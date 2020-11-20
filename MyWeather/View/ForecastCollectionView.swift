a12cc7e7296d9d2eef4909ff797f68b8a269b960
Chris <chrisbanning510@gmail.com>
2020-11-20 15:25:30 -0500

    remove files

---
+++ MyWeather/View/ForecastCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  ForecastCollectionView.swift
+//  MyWeather
+//
+//  Created by chris on 11/14/20.
+//
+
+import UIKit
+
+class ForecastCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ MyWeather/View/ForecastCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  ForecastCollectionViewCell.swift
+//  MyWeather
+//
+//  Created by chris on 11/14/20.
+//
+
+import UIKit
+
+class ForecastCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var forecastTImeLabel: UILabel!
+    @IBOutlet weak var forecastImage: UIImageView!
+    @IBOutlet weak var forecastTempLabel: UILabel!
+    
+}
